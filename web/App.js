25cf0810de7516d23d2847153b6c196d7a0cec6b
ernie <ernie@ottawa.ca>
2021-09-22 17:23:56 -0400

    background.jpg

--- web/App.js
+++ web/App.js
@@ -10,12 +10,12 @@ Vue.use(Vuetify);
 
 const store = new Vuex.Store({
     state: {
-        maxLevel: null,            
+        maxLevel: null,
     },
     mutations: {
         setMaxLevel(state, level) {
-            state.maxLevel = level;        
-        }    
+            state.maxLevel = level;
+        }
     }
 })
 
@@ -29,17 +29,14 @@ new Vue({
           <v-main>
             <v-container fluid>
 
-               <v-toolbar
-                  dark
-                  src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-               >
-                  <v-toolbar-title class="ml-4">Septic Monitor</v-toolbar-title>
+               <v-toolbar src="/background.jpg">
+                  <v-toolbar-title class="ml-4">SepMon</v-toolbar-title>
                   <v-spacer></v-spacer>
                   <v-dialog v-model="settingsOpen" fullscreen hide-overlay>
                     <template v-slot:activator="{on, attrs}">
                       <v-btn icon v-bind="attrs" v-on="on">
                           <v-icon>mdi-dots-vertical</v-icon>
-                      </v-btn>                      
+                      </v-btn>
                     </template>
                     <SettingsDialog :settings-open.sync="settingsOpen"></SettingsDialog>
                   </v-dialog>
@@ -49,13 +46,13 @@ new Vue({
                     <v-col>
                         <v-card elevation="2" shaped class="py-8">
                             <LastUpdate></LastUpdate>
-                        </v-card>        
+                        </v-card>
                     </v-col>
                     <v-col><LevelGauge></LevelGauge></v-col>
                 </v-row>
 
                 <div class="mt-12 mb-6 text-center text--secondary"><h2>Water Levels</h2></div>
-                
+
                 <div>
                     <v-tabs v-model="tab">
                         <v-tab>Hour</v-tab>
@@ -63,16 +60,16 @@ new Vue({
                         <v-tab>Week</v-tab>
                         <v-tab>Month</v-tab>
                     </v-tabs>
-                    
+
                     <v-tabs-items v-model="tab">
                         <v-tab-item><LevelChart duration="hour"></LevelChart></v-tab-item>
                         <v-tab-item><LevelChart duration="day"></LevelChart></v-tab-item>
                         <v-tab-item><LevelChart duration="week"></LevelChart></v-tab-item>
                         <v-tab-item><LevelChart duration="month"></LevelChart></v-tab-item>
                     </v-tabs-items>
-                
+
                 </div>
-                    
+
             </v-container>
           </v-main>
         </v-app>
