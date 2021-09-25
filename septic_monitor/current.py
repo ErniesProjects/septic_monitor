088abe8b1cee3665c1a20031745c6a896bd6de6e
ernie <ernie@brandon.com>
2021-09-25 08:36:44 -0500

    working

--- septic_monitor/current.py
+++ septic_monitor/current.py
@@ -1,26 +1,62 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
+#Read pump AC current while pump is running
+#Monitors 120VAC powering pump
 
+import RPi.GPIO as GPIO
+
+import signal  
+import sys
 import time
+
 import board
 import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-# Create the I2C bus
+PUMP_RUNNING_GPIO = 16
+PUMP_AC_POWER_GPIO = 12
+LED_GPIO = 20
+
 i2c = busio.I2C(board.SCL, board.SDA)
-
-# Create the ADC object using the I2C bus
 ads = ADS.ADS1015(i2c)
-
-# Create single-ended input on channel 0
 chan = AnalogIn(ads, ADS.P0)
 
-# Create differential input between channel 0 and 1
-# chan = AnalogIn(ads, ADS.P0, ADS.P1)
+def signal_handler(sig, frame):
+    GPIO.cleanup()
+    sys.exit(0)
 
-print("{:>5}\t{:>5}".format("raw", "v"))
+def pump_AC_callback(channel):
+    print("Pump AC Fail")
 
-while True:
-    print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    time.sleep(0.5)
+def pump_current_callback(channel):
+    print("Pump Running")
+    print("{:>5}\t{:>5}".format("-Raw-", "AC Current"))
+    
+    PUMP_STATE = 1
+    
+    while PUMP_STATE == 1:
+        PUMP_STATE = GPIO.input(PUMP_RUNNING_GPIO)
+        print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
+        time.sleep(2)
+
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    
+    GPIO.setup(PUMP_RUNNING_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(PUMP_AC_POWER_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(LED_GPIO, GPIO.OUT)
+    
+    GPIO.add_event_detect(PUMP_RUNNING_GPIO, GPIO.RISING, 
+            callback=pump_current_callback, bouncetime=50)
+    
+    GPIO.add_event_detect(PUMP_AC_POWER_GPIO, GPIO.FALLING, 
+            callback=pump_AC_callback, bouncetime=50)
+    
+    signal.signal(signal.SIGINT, signal_handler)
+
+# Do something!
+
+    while True:
+        GPIO.output(LED_GPIO, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(LED_GPIO, GPIO.LOW)
+        time.sleep(0.5)
