870ef6611af84707974dbd680d9bc2f07e783e93
ernie <ernie@ontario.ca>
2021-12-26 10:58:49 -0600

    SepMon.json

--- septic_monitor/mock.py
+++ septic_monitor/mock.py
@@ -14,27 +14,11 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def check_level():
-    t = np.linspace(0, 1, 5000)
-    w = signal.sawtooth(2 * np.pi * 5 * t)
-    for v in w.tolist():
-        dither = random.randrange(-100, 100) / 100.0
-        yield -20 + (v * 10) + dither
-
-
 def back_data():
-    now = datetime.now()
-    level_poll_int = storage.get_tank_level_poll()
-    start = now - timedelta(days=60)
-    logger.info("Creating back-data...")
-    timestamp = start + timedelta(minutes=level_poll_int)
-    for level in itertools.cycle(check_level()):
-        storage.set_tank_level(level, ts=timestamp)
-        if timestamp < now:
-            timestamp = timestamp + timedelta(minutes=5)
-        else:
-            timestamp = datetime.now()
-            time.sleep(level_poll_int * 60)
+    t = datetime.now()
+    for i in range(60):
+        t = t - timedelta(days=i)
+        storage.set_tank_level(40 - (4 * t.weekday()))
 
 
 def main():
@@ -42,11 +26,11 @@ def main():
         back_data()
 
     while True:
-        storage.set_tank_level(random.choice(range(20, 40)))
+        storage.set_tank_level(40 - (4 * datetime.now().weekday()))
         storage.set_pump_amperage(random.choice(
             [0,] * 30 + [random.choice(range(11, 13)),]
         ))
-        time.sleep(15)
+        time.sleep(60)
 
 
 if __name__ == "__main__":
